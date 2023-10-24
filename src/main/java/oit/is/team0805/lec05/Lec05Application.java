c2527d6389c33a5b292a2992a2519a900e55f25f
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-24 14:05:03 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team0805/lec05/Lec05Application.java
@@ -0,0 +1,13 @@
+package oit.is.team0805.lec05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec05Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team0805/lec05/Lec05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team0805.lec05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
