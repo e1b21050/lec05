a53f8e2d50e34baa204d579f2ebc45dfa44ffad8
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-24 14:16:16 +0900

    認証処理の実装

---
+++ src/main/java/oit/is/team0805/lec05/security/Sample3AuthConfiguration.java
@@ -0,0 +1,58 @@
+package oit.is.team0805.lec05.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration {
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   *
+   * @return
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // ユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
+    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
+    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
+    // $ sshrun htpasswd -nbBC 10 user1 p@ss
+
+    // $ sshrun htpasswd -nbBC 10 customer1 p@ss
+    UserDetails customer1 = User.withUsername("cust1")
+        .password("{bcrypt}$2y$10$s9lPQYC3Xl7VLMXRdIOkM.GE4b3SfOS0Sir9iaaGvYYhVzcH1HkOu")
+        .roles("CUSTOMER")
+        .build();
+    // $ sshrun htpasswd -nbBC 10 customer2 p@ss
+    UserDetails customer2 = User.withUsername("cust2")
+        .password("{bcrypt}$2y$10$s9lPQYC3Xl7VLMXRdIOkM.GE4b3SfOS0Sir9iaaGvYYhVzcH1HkOu")
+        .roles("CUSTOMER")
+        .build();
+    // $ sshrun htpasswd -nbBC 10 seller p@ss
+    UserDetails seller = User.withUsername("sell")
+        .password("{bcrypt}$2y$10$s9lPQYC3Xl7VLMXRdIOkM.GE4b3SfOS0Sir9iaaGvYYhVzcH1HkOu")
+        .roles("SELLER")
+        .build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(customer1, customer2, seller);
+  }
+
+}
