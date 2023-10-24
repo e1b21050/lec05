fea9aff255f48ca6d9c715cc5aa9e910c06c7fb6
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-24 15:57:01 +0900

    ブラウザで指定のページを開いている限りカウントアップする数字を表示する処理を実装

--- src/main/java/oit/is/team0805/lec05/Lec05Application.java
+++ src/main/java/oit/is/team0805/lec05/Lec05Application.java
@@ -2,12 +2,16 @@ package oit.is.team0805.lec05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Lec05Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lec05Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Lec05Application.class, args);
+  }
 
 }
